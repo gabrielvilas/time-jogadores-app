f676ef9d948e03bd41ded44823dc385334024168
gabriel vilas <vilasboasgabriel62@gmail.com>
2022-05-30 15:20:20 -0300

    h8hohio

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>cabeçalho</header>
+  <main>Main</main>
+  <footer>rodapê</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+  height: 100%;
+
+
+  font-weight: normal;
+}
+header,
+footer {
+  background-color: aquamarine;
+  color: red;
+  font-size: ;
+}
+</style>
